0e2eba89653d71be4ccd764812ff9622b4e8b92f
Camilo Di Paolo <camilodipaolo10@gmail.com>
2022-12-26 14:03:39 -0300

    first commit

---
+++ src/routes/+page.ts
@@ -0,0 +1,8 @@
+import type { PageLoad } from './$types';
+import { supabase } from '$lib/supabaseClient';
+
+export const load: PageLoad = async () => {
+	const { data } = await supabase.from('juancito table').select('*');
+
+	return { data: data };
+};
