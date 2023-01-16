0146c01cfb58cad7486f30bf23e3404aa0f1a8b2
Camilo Di Paolo <camilodipaolo10@gmail.com>
2023-01-16 11:07:55 -0300

    arreglados los tooltips

--- src/lib/components/Sidebar/SidebarButton.svelte
+++ src/lib/components/Sidebar/SidebarButton.svelte
@@ -18,11 +18,12 @@
 	{href}
 >
 	<Icon src={icon} class="w-8 h-8" />
-	<p
-		class="absolute top-1/2 -translate-y-1/2 left-2/3 translate-x-7 bg-stone-900 text-stone-300 p-1 rounded-md text-[12px] w-max dark:bg-black before:content-[''] before:w-0 before:h-0 before:absolute before:ml-[-14px] before:rotate-[270deg] before:border before:border-b-[6px] before:border-b-stone-900 before:border-r-[5px] before:border-r-transparent before:border-l-[5px] before:border-l-transparent before:border-t-transparent before:dark:border-b-black before:top-1/2 before:-translate-y-1/2 before:left-0 before:translate-x-2/3"
-		transition:fade={{ duration: 50 }}
-	>
-		{tooltip}
-	</p>
-	{#if active}{/if}
+	{#if active}
+		<p
+			class="absolute top-1/2 -translate-y-1/2 left-2/3 translate-x-7 bg-stone-900 text-stone-300 p-1 rounded-md text-[12px] w-max dark:bg-black before:content-[''] before:w-0 before:h-0 before:absolute before:ml-[-14px] before:rotate-[270deg] before:border before:border-b-[6px] before:border-b-stone-900 before:border-r-[5px] before:border-r-transparent before:border-l-[5px] before:border-l-transparent before:border-t-transparent before:dark:border-b-black before:top-1/2 before:-translate-y-1/2 before:left-0 before:translate-x-2/3"
+			transition:fade={{ duration: 50 }}
+		>
+			{tooltip}
+		</p>
+	{/if}
 </a>
