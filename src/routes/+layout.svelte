950a007544a18913be506efd6a742fb0356a0218
Camilo Di Paolo <camilodipaolo10@gmail.com>
2023-02-02 09:25:18 -0300

    Agregado el login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
 	import '../app.css';
 	import Sidebar from '$lib/components/Sidebar/Sidebar.svelte';
+	import { supabase } from '$lib/supabaseClient';
+	import { invalidate, invalidateAll } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidateAll();
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
 <main
